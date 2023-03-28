85373987e9a925098fed12f97771c932d5335357
Valeria Serci <balerty84@gmail.com>
2023-03-28 12:35:29 +0100

    test drive addNote

--- resources/notes-app/notesModel.test.js
+++ resources/notes-app/notesModel.test.js
@@ -4,5 +4,10 @@ describe('NotesModel', () => {
   it('returns an empty list', () => {
     const model = new NotesModel();
     expect(model.getNotes()).toEqual([]);
-  })
+  });
+  it('adds a note', () => {
+    const model = new NotesModel();
+    model.addNote('Note1');
+    expect(model.getNotes()).toEqual(['Note1']);
+  });
 })
